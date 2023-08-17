aa78ae294e373d0407df3c318b85767a8781cf52
Phichaphop Raqsaboon <105978782+Ponynie@users.noreply.github.com>
2023-08-17 17:05:07 +0700

    gcd.py

--- lab 1/gcd.py
+++ lab 1/gcd.py
@@ -1,42 +1,4 @@
-import timeit
-import matplotlib.pyplot as plt
-from math import sqrt
-
-def FindGCD1(m, n):
-    t = min(m, n)
-    while True:
-        if m % t == 0 and n % t == 0:
-            return t
-        t = t - 1
-
-def FindGCD2(m, n):
-    m_factor = _prime_factorization(m)
-    n_factor = _prime_factorization(n)
-    common_factor = m_factor.intersection(n_factor)
-    product = 1
-    for i in common_factor:
-        product *= i
-    return product
-
-def FindGCD3(m, n):
-    if m > n:
-        return FindGCD3(m - n, n)
-    elif m < n:
-        return FindGCD3(m, n - m)
-    else:
-        return m 
-
-def _prime_factorization(n):
-    factor = set()
-    for i in range(2, n + 1):
-        if n % i == 0: 
-            factor.add(i)
-    for i in list(factor):
-        for j in range(2, int(i/2) + 1):
-            if i % j == 0:
-                factor.remove(i)
-                break
-    return factor
+deprecated
 
 input_list = [(5, 7), (7, 10), (10, 20)]
 time_list_I = []
