931b59bc12fbca722e4592459f2af08210f376ed
Phichaphop Raqsaboon <105978782+Ponynie@users.noreply.github.com>
2023-08-28 20:12:32 +0700

    added prelab3

---
+++ lab3/check_bruteforce.py
@@ -0,0 +1,44 @@
+f = open("3.3.1.txt","r")  
+array = []
+a = f.readline().strip()
+for i in a:
+    array.append(i)
+k = int(f.readline())
+f.close()
+
+# Brute Force technique
+def BruteForce(ar,output):
+    global count
+      
+    for i in range(len(ar)):
+        if ar[i] == 'G':
+            if i-k < 0:
+                a = 0
+            else:
+                a = i-k
+            if i+k > len(array)-1:
+                b = len(array)-1
+            else:
+                b = i+k
+            while a <= b:
+                if a != i: 
+                    if ar[a] == 'P':
+                        ar[a] = None
+                        ar[i] = None
+                        output = output+1
+                        BruteForce(ar,output)
+                        ar[a] == 'P'
+                        ar[i] == 'G'
+                a=a+1
+    if output > maxnum:
+        output = maxnum
+    if output == maxnum:
+        count = count + 1      
+
+ar = array
+maxnum = 0
+output = 0
+count = 0
+BruteForce(ar,output)
+print("count max:", count)    
+    
---
+++ lab3/check_greedy.py
@@ -0,0 +1,52 @@
+f = open("3.3.1.txt","r")  
+array = []
+a = f.readline().strip()
+for i in a:
+    array.append(i)
+k = int(f.readline())
+f.close()
+
+# Greedy technique
+output = 0
+for i in range(len(array)):
+    n = 0
+    found = False
+    if array[i] == 'G':
+        if i-k < 0:
+            a = 0
+        else:
+            a = i-k
+        if i+k > len(array)-1:
+            b = len(array)-1
+        else:
+            b = i+k
+        r = b-a
+        while n < r and found == False: 
+             
+            if n % 2 == 0 and a != i: 
+                if array[a] == 'P':
+                    array[a] = None
+                    output=output+1
+                    found = True  # if 'G' can find 'P' then break
+                a=a+1
+            elif n % 2 != 0 and a!= i:
+                if array[a] == 'P':
+                    array[a] = None
+                    output=output+1
+                    found = True  
+                a=a+1
+            elif n % 2 == 0 and b!= i:
+                if array[b] == 'P':
+                    array[b] = None
+                    output=output+1
+                    found = True
+                b=b-1
+            elif n % 2 != 0 and b!= i:
+                if array[b] == 'P':
+                    array[b] = None
+                    output=output+1
+                    found = True
+                b=b-1 
+             
+            n=n+1       
+print("output:",output)
