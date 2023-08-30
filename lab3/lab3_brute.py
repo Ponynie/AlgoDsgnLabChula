11c6f21eb48df466a3a7324bd91fc472cac2ee8b
Phichaphop Raqsaboon <105978782+Ponynie@users.noreply.github.com>
2023-08-30 19:05:39 +0700

    added comment

--- lab3/lab3_brute.py
+++ lab3/lab3_brute.py
@@ -2,34 +2,36 @@ import itertools
 
 file_path = "lab3/test.txt"
 
-with open(file_path, 'r') as file:
-    first_line = file.readline().strip()
+with open(file_path, 'r') as file: #read file
+    first_line = file.readline().strip() 
     data = list(first_line) 
     second_line = file.readline().strip()  
     distance = int(second_line) 
 
-passenger = [i for i in range(len(data)) if data[i] == 'P']
+passenger = [i for i in range(len(data)) if data[i] == 'P'] 
 car = [i for i in range(len(data)) if data[i] == 'G']
 
-def configuration_is_possible(configuration, distance):
+def configuration_is_possible(configuration, distance): #check if configuration is possible
     for pair in configuration:
         if abs(pair[0] - pair[1]) > distance:
             return False
     return True
 
-solutions = []
-for i in range(1, len(car) + 1):
-    car_i_permutations = list(itertools.permutations(car, i))
-    passenger_i_combinations = list(itertools.combinations(passenger, i))
-    for car_i_permutation in car_i_permutations:
-        for passenger_i_combination in passenger_i_combinations:
+solutions = [] #list of solutions
+
+for i in range(1, len(car) + 1): #generate all possible configurations
+    car_i_permutations = list(itertools.permutations(car, i)) #generate all possible permutations of cars when choose i cars
+    passenger_i_combinations = list(itertools.combinations(passenger, i)) #generate all possible combinations of passengers when choose i passengers
+    for car_i_permutation in car_i_permutations: 
+        for passenger_i_combination in passenger_i_combinations: #for each permutation of cars and each combination of passengers
             configuration = [(car_i_permutation[i], passenger_i_combination[i]) for i in range(len(car_i_permutation))]
-            if configuration_is_possible(configuration, distance): 
-                solutions.append(configuration)
+            if configuration_is_possible(configuration, distance): #check if configuration is possible
+                solutions.append(configuration) #add configuration to list of solutions
             
                 
 for solution in solutions:
-    print(f"One of the solutions is {solution}")
+    print(f"One of the solutions is {solution}") #print all solutions
 
-number_of_max_solution = len(max(solutions, key=len))
-print(f"Number of all maximum solutions: {number_of_max_solution}")
+max_passenger_value = len((max(solutions, key=len))) #find max number of passengers
+number_of_max_solution = sum(1 for solution in solutions if len(solution) == max_passenger_value) #count number of solutions with max number of passengers
+print(f"Number of all maximum solutions: {number_of_max_solution}") #print number of solutions with max number of passengers
