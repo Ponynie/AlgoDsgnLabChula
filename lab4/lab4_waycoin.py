bf3a4ff850c49435509a0c5f08f4cd5bac4f3a5d
Phichaphop Raqsaboon <105978782+Ponynie@users.noreply.github.com>
2023-09-08 02:36:58 +0700

    LAB4 FINAL VERSION 🎉

--- lab4/lab4_waycoin.py
+++ lab4/lab4_waycoin.py
@@ -1,17 +1,22 @@
 import numpy as np
+import copy
 
-file_path = "lab4/test_case/4.7.txt"
+file_path = "lab4/test_case/4.11.txt"
 
 with open(file_path, 'r') as file: 
     amount = int(file.readline().strip())
     denominations = sorted(list(map(int, file.readline().strip().split(sep = " "))))
-    
+
+ways_solution = [[None for _ in range(amount + 2)] for _ in range(len(denominations))]
+
 def ways_coin_change_topdown(amount: int, denominations: list) -> int:
     ways_value = np.full((len(denominations), amount + 1), None)
+    #ways_solution = [[None for _ in range(amount + 1)] for _ in range(len(denominations))]
     return _ways_coin_change(len(denominations) - 1, amount, denominations, ways_value)
 
 def _ways_coin_change(denomidex: int, amount: int, denominations: list, ways_value: np.array) -> int:
     if amount == 0 and denomidex >= 0:
+        ways_solution[denomidex][amount] = [[],]
         return 1
     elif denomidex < 0 or amount < 0:
         return 0
@@ -20,6 +25,23 @@ def _ways_coin_change(denomidex: int, amount: int, denominations: list, ways_val
             return ways_value[denomidex][amount]
         else:
             ways_value[denomidex][amount] = _ways_coin_change(denomidex - 1, amount, denominations, ways_value) + _ways_coin_change(denomidex, amount - denominations[denomidex], denominations, ways_value)
+            
+            #Recursively construct the solution and saved in global variable
+            right_solutions = ways_solution[denomidex][amount - denominations[denomidex]]
+            left_solutions = ways_solution[denomidex - 1][amount]
+            if right_solutions != None:
+                right_solutions = copy.deepcopy(ways_solution[denomidex][amount - denominations[denomidex]])
+                for solution in right_solutions: solution.append(denominations[denomidex])
+            if  left_solutions != None:
+                left_solutions = copy.deepcopy(ways_solution[denomidex - 1][amount]) 
+            if right_solutions != None and left_solutions != None:
+                ways_solution[denomidex][amount] = right_solutions + left_solutions
+            elif right_solutions != None and left_solutions == None:
+                ways_solution[denomidex][amount] = right_solutions
+            elif right_solutions == None and left_solutions != None:
+                ways_solution[denomidex][amount] = left_solutions
+            
+            #Return the number of ways
             return ways_value[denomidex][amount]
 
 def ways_coin_change_bottomup(amount: int, denominations: list) -> int:
@@ -31,5 +53,14 @@ def ways_coin_change_bottomup(amount: int, denominations: list) -> int:
             ways_value[denomidex][m] = ways_value[denomidex - 1][m] + ways_value[denomidex][m - denominations[denomidex]]
     return int(ways_value[len(denominations) - 1][amount])
 
-#print(f"The number of ways to make change for {amount} units given {denominations} is {ways_coin_change_topdown(amount, denominations)} (usign topdown approach)")
-print(f"The number of ways to make change for {amount} units given {denominations} is {ways_coin_change_bottomup(amount, denominations)} (usign bottomup approach)")
+print(f"Runing {file_path} ...")
+choice = input("Do you want to print the solution? (y/n): ")
+print("------------------------------------------------------------------------------------------------")
+if choice == "y":
+    print(f"The number of ways to make change for {amount} units given {denominations} is {ways_coin_change_topdown(amount, denominations)} (usign topdown approach)")
+    if ways_solution[len(denominations) - 1][amount] != None: 
+        for i in ways_solution[len(denominations) - 1][amount]: 
+            if sum(i) <= amount: print(i)
+else:
+    print(f"The number of ways to make change for {amount} units given {denominations} is {ways_coin_change_bottomup(amount, denominations)} (usign bottomup approach)")
+print("------------------------------------------------------------------------------------------------")
