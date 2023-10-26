580a1066f0313511d5910f3789d9bee0e773e8c6
Phichaphop Raqsaboon <105978782+Ponynie@users.noreply.github.com>
2023-10-26 11:41:30 +0700

    added

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -5,7 +5,7 @@ def lab7():
     def main():
         with open("lab7/test_case/7.1.txt", "r") as f:
             lines = f.readlines()
-            num_vertex, num_edge, num_query = [int(i) for i in lines[0].strip().split()]
+            num_vertex, num_edge = [int(i) for i in lines[0].strip().split()][0:2]
             edge_array = [tuple(map(int, line.strip().split())) for line in lines[1:num_edge+1]]
             query_array = [tuple(map(int, line.strip().split())) for line in lines[num_edge+1:]]
             graph_obj = Graph.construct_graph(num_vertex, edge_array)
